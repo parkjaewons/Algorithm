be8e5a15ae667975e5adc866e22daa9ff81ac526
ParkGamdol <94657194+parkjaewons@users.noreply.github.com>
2023-11-16 17:30:52 +0900

    [level 1] Title: 행렬의 덧셈, Time: 4.10 ms, Memory: 94.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(arr1: Array<IntArray>, arr2: Array<IntArray>): Array<IntArray> {
+        return Array<IntArray>(arr1.size){ i ->
+            IntArray(arr1[0].size){ j -> arr1[i][j] + arr2[i][j] }
+        }
+    }
+}

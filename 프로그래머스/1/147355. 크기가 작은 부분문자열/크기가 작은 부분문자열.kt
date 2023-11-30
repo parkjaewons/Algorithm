9488a084c53f20f133f0ffe645586c2135118176
박재원 <94657194+parkjaewons@users.noreply.github.com>
2023-11-30 10:00:16 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.13 ms, Memory: 62.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun solution(t: String, p: String): Int {
+        var answer: Int = 0
+        
+        for(i in 0 until t.length - p.length+1){
+            val s = t.substring(i, i + p.length)
+            
+            if(s.toLong() <= p.toLong()){
+                answer++
+            }
+        }
+        return answer
+    }
+    
+}

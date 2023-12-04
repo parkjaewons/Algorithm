80c49f69d0e54052e074150c3a8e8eccb53359c5
박재원 <94657194+parkjaewons@users.noreply.github.com>
2023-12-04 09:59:35 +0900

    [level 1] Title: 시저 암호, Time: 18.97 ms, Memory: 64 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(s: String, n: Int): String {
+        return s.toList().joinToString(separator = "") {
+            when (it) {
+                in 'A'..'Z' -> ('A'.toInt() + (it.toInt() - 'A'.toInt() + n) % ('Z' - 'A' + 1)).toChar()
+                in 'a'..'z' -> ('a'.toInt() + (it.toInt() - 'a'.toInt() + n) % ('z' - 'a' + 1)).toChar()
+                else -> it
+            }.toString()
+        }
+    }
+}
